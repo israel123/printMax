057920178d7435c305f73abb5b74f4c3abcb68fd
israel david dahan <id@studiodvd.co.il>
2014-12-15 19:54:57 +0200

    add files

---
+++ src/project2/printMax.java
@@ -0,0 +1,19 @@
+package project2;
+
+public class printMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int x = 3, y = 5;
+		 
+		
+		if(x>y){
+			System.out.println("X is the bigger value:");
+		}
+		else{
+			System.out.println("Y is the bigger value:");
+			System.out.println("Becouse the value of Y=" + y +  " and x is value=" + x);
+	}
+	}
+}
+
